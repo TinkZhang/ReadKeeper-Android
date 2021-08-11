9ef6f2f4ebc4251643105596b8ce30b445a9e2ad
Tink, Zhang <ZhangYunfengZJU@gmail.com>
2021-08-11 18:54:09 +0800

    add topbar

--- app/src/main/java/com/github/tinkzhang/readkeeper/MainActivity.kt
+++ app/src/main/java/com/github/tinkzhang/readkeeper/MainActivity.kt
@@ -3,14 +3,32 @@ package com.github.tinkzhang.readkeeper
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
@@ -33,39 +51,8 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
                     Scaffold(
-                        bottomBar = {
-                            BottomNavigation {
-                                val navBackStackEntry by navController.currentBackStackEntryAsState()
-                                val currentDestination = navBackStackEntry?.destination
-                                getBottomBarItemList().forEach { screen ->
-                                    BottomNavigationItem(
-                                        selected = currentDestination?.hierarchy?.any { it.route == screen.route } == true,
-                                        icon = {
-                                            Icon(
-                                                screen.icon,
-                                                contentDescription = stringResource(id = screen.labelId),
-                                            )
-                                        },
-                                        label = { Text(stringResource(id = screen.labelId)) },
-                                        onClick = {
-                                            navController.navigate(screen.route) {
-                                                // Pop up to the start destination of the graph to
-                                                // avoid building up a large stack of destinations
-                                                // on the back stack as users select items
-                                                popUpTo(navController.graph.findStartDestination().id) {
-                                                    saveState = true
-                                                }
-                                                // Avoid multiple copies of the same destination when
-                                                // reselecting the same item
-                                                launchSingleTop = true
-                                                // Restore state when reselecting a previously selected item
-                                                restoreState = true
-                                            }
-                                        },
-                                    )
-                                }
-                            }
-                        }
+                        topBar = { TopBar() },
+                        bottomBar = { BottomBar(navController = navController) },
                     ) {
                         NavHost(navController, startDestination = SCREEN_ROUTE.HOME) {
                             composable(SCREEN_ROUTE.HOME) {
@@ -89,4 +76,100 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+}
+
+@Composable
+fun BottomBar(navController: NavHostController) {
+    BottomNavigation {
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentDestination = navBackStackEntry?.destination
+        getBottomBarItemList().forEach { screen ->
+            BottomNavigationItem(
+                selected = currentDestination?.hierarchy?.any { it.route == screen.route } == true,
+                icon = {
+                    Icon(
+                        screen.icon,
+                        contentDescription = stringResource(id = screen.labelId),
+                    )
+                },
+                label = { Text(stringResource(id = screen.labelId)) },
+                onClick = {
+                    navController.navigate(screen.route) {
+                        // Pop up to the start destination of the graph to
+                        // avoid building up a large stack of destinations
+                        // on the back stack as users select items
+                        popUpTo(navController.graph.findStartDestination().id) {
+                            saveState = true
+                        }
+                        // Avoid multiple copies of the same destination when
+                        // reselecting the same item
+                        launchSingleTop = true
+                        // Restore state when reselecting a previously selected item
+                        restoreState = true
+                    }
+                },
+            )
+        }
+    }
+}
+
+@Composable
+fun TopBar(modifier: Modifier = Modifier.fillMaxWidth()) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween,
+        modifier = modifier,
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+
+            ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                contentDescription = "",
+                modifier = Modifier.size(64.dp)
+            )
+            ReadKeeperBarTitleText()
+        }
+        ProfileImage(modifier = Modifier.padding(end = 16.dp))
+    }
+}
+
+@Composable
+fun ReadKeeperBarTitleText() {
+    Text(buildAnnotatedString {
+        withStyle(style = SpanStyle(fontWeight = FontWeight.Bold, color = Color(0xffdb4437))) {
+            append("R")
+        }
+        withStyle(style = SpanStyle(fontWeight = FontWeight.Bold, color = Color(0xff4285f4))) {
+            append("eed")
+        }
+        withStyle(style = SpanStyle(fontWeight = FontWeight.Bold, color = Color(0xfff4b400))) {
+            append("K")
+        }
+        withStyle(style = SpanStyle(fontWeight = FontWeight.Bold, color = Color(0xff0f9d58))) {
+            append("eeper")
+        }
+    }, fontSize = 22.sp)
+}
+
+@Composable
+fun ProfileImage(
+    modifier: Modifier = Modifier,
+    onClickAction: () -> Unit = {},
+) {
+    Image(
+        painter = painterResource(id = R.drawable.ic_profile_24),
+        contentDescription = "Profile",
+        modifier = modifier
+            .size(36.dp)
+            .clip(CircleShape)
+            .clickable { onClickAction },
+    )
+}
+
+@Preview
+@Composable
+fun TopBarPreview() {
+    TopBar()
 }
