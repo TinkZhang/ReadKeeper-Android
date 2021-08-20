d2477b54718e15be9c45450906849f2cdcd67652
Tink, Zhang <ZhangYunfengZJU@gmail.com>
2021-08-20 16:44:36 +0800

    try to sent instabug color, but does not work for free plan

--- app/src/main/java/com/github/tinkzhang/readkeeper/instabug/InstabugWrapper.kt
+++ app/src/main/java/com/github/tinkzhang/readkeeper/instabug/InstabugWrapper.kt
@@ -7,7 +7,6 @@ import com.instabug.bug.invocation.Option
 import com.instabug.library.Instabug
 import com.instabug.library.invocation.InstabugInvocationEvent
 
-
 object InstabugWrapper {
     var enabled: Boolean = true
     private var initialized: Boolean = false
@@ -15,10 +14,11 @@ object InstabugWrapper {
         if (enabled) {
             val token = application.getString(R.string.instabug_token)
             Instabug.Builder(application, token)
-                .setInvocationEvents(InstabugInvocationEvent.NONE)
+                .setInvocationEvents(InstabugInvocationEvent.SHAKE, InstabugInvocationEvent.SCREENSHOT)
                 .build()
+            Instabug.setPrimaryColor(application.getColor(R.color.primaryColor))
             BugReporting.setOptions(Option.EMAIL_FIELD_OPTIONAL)
-            BugReporting.setAttachmentTypesEnabled(false, true, true, true)
+            BugReporting.setAttachmentTypesEnabled(false, false, false, false)
             initialized = true
         }
     }
