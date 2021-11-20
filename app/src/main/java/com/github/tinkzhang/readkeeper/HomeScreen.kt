8ef92ec8b3669ccbc291a62f6882a67c595fe7c3
Tink, Zhang <ZhangYunfengZJU@gmail.com>
2021-11-20 11:01:52 +0800

    update Search box color:

--- app/src/main/java/com/github/tinkzhang/readkeeper/HomeScreen.kt
+++ app/src/main/java/com/github/tinkzhang/readkeeper/HomeScreen.kt
@@ -5,12 +5,14 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.Button
-import androidx.compose.material3.Icon
 import androidx.compose.material.OutlinedTextField
-import androidx.compose.material3.Text
+import androidx.compose.material.TextFieldDefaults
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Search
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -22,14 +24,16 @@ import com.github.tinkzhang.readkeeper.search.SearchViewModel
 
 
 @Composable
-fun HomeScreen(navController: NavController,
-               searchViewModel: SearchViewModel,
-               readingViewModel: ReadingViewModel,
+fun HomeScreen(
+    navController: NavController,
+    searchViewModel: SearchViewModel,
+    readingViewModel: ReadingViewModel,
 ) {
     var searchKeyword by remember {
         mutableStateOf("")
     }
     Column() {
+        // TODO: Update to M3 version TextField when it's available
         OutlinedTextField(
             value = searchKeyword,
             onValueChange = { text -> searchKeyword = text },
@@ -46,6 +50,11 @@ fun HomeScreen(navController: NavController,
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(16.dp),
+            colors = TextFieldDefaults.outlinedTextFieldColors(
+                backgroundColor = MaterialTheme.colorScheme.secondaryContainer,
+                unfocusedBorderColor = MaterialTheme.colorScheme.secondary,
+                focusedBorderColor = MaterialTheme.colorScheme.primary,
+            )
         )
         Button(onClick = {
             readingViewModel.addBook()
@@ -56,7 +65,7 @@ fun HomeScreen(navController: NavController,
         Button(onClick = {
             readingViewModel.syncList()
         }) {
-           Text("Get Book")
+            Text("Get Book")
         }
     }
 
