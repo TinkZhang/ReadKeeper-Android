fdbb50002dba2be5735990bcc96c1553f40ad788
Tink Zhang <ZhangYunfengZJU@gmail.com>
2021-12-21 22:03:01 +0800

    [25]: add reading vip

--- app/src/main/java/com/github/tinkzhang/readkeeper/reading/ReadingItemScreen.kt
+++ app/src/main/java/com/github/tinkzhang/readkeeper/reading/ReadingItemScreen.kt
@@ -1,8 +1,9 @@
 package com.github.tinkzhang.readkeeper.reading
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.Card
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Divider
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.icons.Icons
@@ -11,16 +12,13 @@ import androidx.compose.material.icons.filled.DataSaverOn
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import coil.compose.rememberImagePainter
 import com.github.tinkzhang.readkeeper.R
-import com.github.tinkzhang.readkeeper.common.data.BookInfo
-import com.github.tinkzhang.readkeeper.common.data.ReadingBook
-import com.github.tinkzhang.readkeeper.common.data.ReadingBookFactory
+import com.github.tinkzhang.readkeeper.reading.uicomponents.RkBookInfoSection
+import com.github.tinkzhang.readkeeper.reading.uicomponents.RkBookNoteSection
+import com.github.tinkzhang.readkeeper.reading.uicomponents.RkBookProgressSection as RkBookProgressSection1
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -31,7 +29,7 @@ fun ReadingItemScreen(
 ) {
     val book = readingViewModel.getBook(uuid)
     Scaffold(topBar = {
-        MediumTopAppBar(title = { Text(book.bookInfo.title) }, navigationIcon = {
+        LargeTopAppBar(title = { Text(book.bookInfo.title) }, navigationIcon = {
             IconButton(onClick = { navController.popBackStack() }) {
                 Icon(
                     Icons.Default.ArrowBack,
@@ -52,62 +50,17 @@ fun ReadingItemScreen(
                 )
             })
     }) {
-        Column(modifier = Modifier.fillMaxWidth()) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp)
+        ) {
             RkBookInfoSection(book = book)
-        }
-    }
-
-}
-
-@Composable
-fun RkBookInfoSection(book: ReadingBook) {
-    Row(modifier = Modifier.fillMaxWidth()) {
-        Card(modifier = Modifier.weight(1.0f)) {
-            Image(
-                painter = rememberImagePainter(
-                    data = book.bookInfo.imageUrl,
-                    builder = {
-                        placeholder(R.drawable.book_sample)
-                    }),
-                contentDescription = null,
-                contentScale = ContentScale.FillWidth,
-            )
-        }
-        RkBookInfoMetadata(book = book.bookInfo, modifier = Modifier.weight(2.0f))
-    }
-}
-
-@Composable
-fun RkBookInfoMetadata(book: BookInfo, modifier: Modifier = Modifier) {
-    Column(
-        modifier = modifier.fillMaxWidth().padding(16.dp),
-        verticalArrangement = Arrangement.SpaceAround
-    ) {
-        if (book.author.isNotEmpty()) {
-            Text(
-                "✍️   Author: " + book.author,
-                style = MaterialTheme.typography.titleLarge
-            )
-        }
-
-        if (book.rating != 0.0) {
-            Text(
-                text = "⭐   Rating: ${book.rating} of 10",
-                style = MaterialTheme.typography.titleLarge
-            )
-        }
-
-        if (book.pubYear != 0) {
-            Text(
-                "\uD83D\uDDD3️   Published in ${book.pubYear}",
-                style = MaterialTheme.typography.titleLarge
-            )
+            Divider(Modifier.padding(vertical = 8.dp))
+            RkBookProgressSection1(book.records, book.pageFormat, book.platform)
+            Divider(Modifier.padding(vertical = 8.dp))
+            RkBookNoteSection(book.notes)
         }
     }
 }
 
-@Preview(showBackground = true, widthDp = 240)
-@Composable
-fun RkBookInfoSectionPreview() {
-    RkBookInfoSection(book = ReadingBookFactory.buildSample())
-}
