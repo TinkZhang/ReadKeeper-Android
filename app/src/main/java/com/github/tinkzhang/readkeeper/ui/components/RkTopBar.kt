16b4093ea20734118ee178b7045b585d3fd16852
Tink Zhang <ZhangYunfengZJU@gmail.com>
2022-05-14 18:02:50 +0800

    [97]: Put the search bar into the app topbar

--- app/src/main/java/com/github/tinkzhang/readkeeper/ui/components/RkTopBar.kt
+++ app/src/main/java/com/github/tinkzhang/readkeeper/ui/components/RkTopBar.kt
@@ -3,14 +3,12 @@ package com.github.tinkzhang.readkeeper.ui.components
 import androidx.compose.foundation.Image
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material3.CenterAlignedTopAppBar
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.clearAndSetSemantics
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
@@ -18,31 +16,45 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.sp
 import com.github.tinkzhang.readkeeper.R
+import com.github.tinkzhang.uicomponent.HomepageSearchBar
+import com.github.tinkzhang.uicomponent.PreviewAnnotation
 import com.github.tinkzhang.uicomponent.RkProfileImage
-
+import com.github.tinkzhang.uicomponent.theme.ReadKeeperTheme
 
 @Composable
 fun RkMainTopBar(
     isLogged: Boolean,
     profileUrl: String = "",
-    onProfileClickAction: () -> Unit = {}
+    onProfileClickAction: () -> Unit = {},
+    onSearchClick: () -> Unit = {}
 ) {
     CenterAlignedTopAppBar(
-        title = { RkTopBarTitleText() },
         navigationIcon = {
             Image(
                 painter = painterResource(id = R.drawable.ic_readkeeperlogo),
                 contentDescription = "",
             )
         },
+        title = {
+            HomepageSearchBar(
+                onClick = onSearchClick
+            )
+        },
         actions = {
             if (isLogged) {
-                IconButton(onClick = onProfileClickAction) {
+                IconButton(
+                    onClick = onProfileClickAction
+                ) {
                     RkProfileImage(profileUrl = profileUrl)
                 }
             } else {
-                IconButton(onClick = onProfileClickAction) {
-                    Icon(imageVector = Icons.Default.Settings, contentDescription = "Settings")
+                IconButton(
+                    onClick = onProfileClickAction
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Settings,
+                        contentDescription = stringResource(id = R.string.settings)
+                    )
                 }
             }
         }
@@ -69,4 +81,14 @@ fun RkTopBarTitleText() {
         fontSize = 22.sp,
         modifier = Modifier.clearAndSetSemantics { }
     )
+}
+
+@PreviewAnnotation
+@Composable
+private fun MainTopBar() {
+    ReadKeeperTheme {
+        Surface {
+            RkMainTopBar(isLogged = true)
+        }
+    }
 }
