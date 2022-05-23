99803dba3bc4364804ebfa780bebe6143805572a
Tink Zhang <ZhangYunfengZJU@gmail.com>
2022-05-23 08:17:22 +0800

    [97]: update search list item ui

--- feature/search/src/main/java/com/github/tinkzhang/search/ui/components/SearchCardMetadata.kt
+++ feature/search/src/main/java/com/github/tinkzhang/search/ui/components/SearchCardMetadata.kt
@@ -1,54 +1,75 @@
 package com.github.tinkzhang.search.ui.components
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.github.tinkzhang.basic.model.SearchBook
 import com.github.tinkzhang.basic.model.SearchBookFactory
+import com.github.tinkzhang.uicomponent.PreviewAnnotation
+import com.github.tinkzhang.uicomponent.ReadingIconToggleButton
+import com.github.tinkzhang.uicomponent.WishIconToggleButton
+import com.github.tinkzhang.uicomponent.theme.ReadKeeperTheme
 
 
 @Composable
 fun SearchCardMetadata(
     book: SearchBook,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    onReadingButtonClicked: (Boolean) -> Unit = {},
+    onWishButtonClicked: (Boolean) -> Unit = {},
 ) {
     Column(
         modifier = modifier
             .fillMaxWidth()
-            .padding(start = 16.dp, end = 8.dp, top = 8.dp, bottom = 8.dp),
+            .padding(start = 16.dp, end = 8.dp, top = 0.dp, bottom = 0.dp),
         verticalArrangement = Arrangement.SpaceBetween,
     ) {
-        Text(
-            book.bookInfo.title,
-            style = MaterialTheme.typography.titleLarge,
-            maxLines = 3,
-            modifier = Modifier.padding(bottom = 8.dp),
-        )
-        Column(verticalArrangement = Arrangement.Bottom) {
+        Column(modifier = Modifier.fillMaxWidth()) {
+            Text(
+                book.bookInfo.title,
+                style = MaterialTheme.typography.titleMedium,
+                maxLines = 3,
+                modifier = Modifier.padding(bottom = 8.dp),
+            )
+            Spacer(modifier = Modifier.height(4.dp))
             Text(
                 "✍️   " + book.bookInfo.author,
-                style = MaterialTheme.typography.titleMedium,
+                style = MaterialTheme.typography.bodyMedium,
                 maxLines = 2,
                 modifier = Modifier.padding(bottom = 8.dp),
             )
+        }
+        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
             Text(
                 "\uD83D\uDCC5   " + book.bookInfo.pubYear.toString(),
-                style = MaterialTheme.typography.titleMedium,
+                style = MaterialTheme.typography.bodyMedium,
                 modifier = Modifier.padding(bottom = 8.dp),
             )
+            Row() {
+                ReadingIconToggleButton(
+                    checked = book.isInReading
+                ) {
+                    onReadingButtonClicked(it)
+                }
+                Spacer(modifier = Modifier.width(16.dp))
+                WishIconToggleButton(checked = book.isInWish) {
+                    onWishButtonClicked(it)
+                }
+            }
         }
     }
 }
 
-@Preview
+@PreviewAnnotation
 @Composable
 private fun SearchCardMetadataPreview() {
-    SearchCardMetadata(book = SearchBookFactory.buildSample())
+    ReadKeeperTheme {
+        Surface {
+            SearchCardMetadata(book = SearchBookFactory.buildSample())
+        }
+    }
 }
